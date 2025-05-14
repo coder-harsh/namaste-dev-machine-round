c8aa9ae4e4ec4f96cb665c640eadc792190cd9d9
Harsh kumar <harshj7.net@gmail.com>
2025-05-15 03:22:45 +0530

    User profile machine question

---
+++ UserProfile.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './styles.css';
+const user = {
+  name: 'Jane Doe',
+  bio: 'Frontend developer who loves React and coffee ☕️',
+  image: 'https://do6gp1uxl3luu.cloudfront.net/question-webp/dummyUser.jpg'
+};
+
+class UserProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showBio: false
+    };
+  }
+  handleBioShow = () => {
+    this.setState(prevState => ({
+      showBio: !prevState.showBio
+    }));
+  }
+  render() {
+    return (
+      <div className="user-profile">
+        <img src={user.image}></img>
+        <h2>{user.name}</h2>
+        <button onClick={this.handleBioShow}>
+          {this.state.showBio ? "Hide Bio" : "Show Bio"}
+        </button>
+        {
+          this.state.showBio && <p>{user.bio}</p>
+        }
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
